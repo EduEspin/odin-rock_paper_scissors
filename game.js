86df970d36f4cfd0faa17d0659589970013b8615
EduEspin <48257753+EduEspin@users.noreply.github.com>
2024-09-03 16:02:32 -0400

    Add playRound and playGame functions

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getRandomNumber1to3(){
     const minNumber = Math.ceil(1);
     const maxNumber = Math.floor(3);
@@ -17,22 +20,91 @@ function getComputerChoice(){
     } 
 }
 
-function getHumanChoice(){
-
-    
-    
-    let humanChoice = prompt("What's your choice?","");
+function getHumanChoice(){  
+    let humanChoice = prompt("What's your choice? \"rock\", \"paper\" or \"scissors\"");
     humanChoice = humanChoice.toLowerCase();
     if (humanChoice === "paper" || humanChoice === "rock" || humanChoice === "scissors"){
         return humanChoice
     }else {
-        alert("Enter a valid choice");
+        alert("Please, enter a valid choice");
         //humanChoice = null;
         return getHumanChoice()
     }
-    
-
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice,computerChoice) {
+
+    if (humanChoice === computerChoice) {
+            return alert("It's a Tie");
+    }else if ((humanChoice === "rock" || computerChoice === "rock") && (humanChoice === "paper" || computerChoice === "paper")) {
+        if (humanChoice === "rock") {
+            computerScore++;
+            return alert("You lose! Paper beats Rock.");
+        }else if (humanChoice === "paper") {
+            humanScore++;
+            return alert("You win! Paper beats Rock.");
+        }
+    } else if ((humanChoice === "rock" || computerChoice === "rock") && (humanChoice === "scissors" || computerChoice === "scissors")) {
+        if (humanChoice === "scissors") {
+            computerScore++;
+            return alert("You lose! Rock beats Scissors.");
+        }else if (humanChoice === "rock") {
+            humanScore++;
+            return alert("You win! Rock beats Scissors.");
+        }
+    } else if ((humanChoice === "paper" || computerChoice === "paper") && (humanChoice === "scissors" || computerChoice === "scissors")) {
+        if (humanChoice === "paper") {
+            computerScore++;
+            return alert("You lose! Scissors beats Paper.");
+        }else if (humanChoice === "scissors") {
+            humanScore++;
+            return alert("You win! Scissors beats Paper.");
+        }
+    }
+}
+function playGame(){
+    
+    for (let i = 0; i < 5; i++) {
+        alert("This is Round #"+(i+1))
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        alert("Computer Choice is " + computerSelection);
+        playRound(humanSelection, computerSelection);
+        console.log("Human Choice is " + humanSelection);
+        console.log("Computer Choice is " + computerSelection);
+        alert("Human Score is " +" "+ humanScore +" "+ "and " + "Computer Score is " +" "+ computerScore);
+        console.log("Human Score is " +" "+ humanScore +" "+ "and " + "Computer Score is " +" "+ computerScore);
+    }
+
+    if (humanScore > computerScore) {
+        alert("Congratulations, you won the game!");
+        
+    }else if (humanScore < computerScore) {
+        alert("Sorry, you lost the game!");
+    }else if (humanScore === computerScore) {
+        alert("You tie the game with the Computer");
+    }
+    x = 0;
+    while (x != 1) {
+        let playAgain = prompt("Would you like to play again?, Type \"yes\" or \"no\"");
+        playAgain.toLowerCase();
+            if (playAgain === "yes") {
+            alert("The new game will start now");  
+            computerScore = 0;
+            humanScore = 0;  
+            return playGame()
+        } else if (playAgain === "no") {
+            alert("Thank's for playing!")
+            computerScore = 0;
+            humanScore = 0;  
+            x = 1;
+        }else{
+            alert("Please, enter a valid choice")
+        }
+    }
+    
+}
+
+playGame();
+
+
